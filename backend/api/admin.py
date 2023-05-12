41bdad829469b6841ad23bc444c179e978ec8b55
Salmaan Saeed <salsaeed999@gmail.com>
2023-05-12 00:24:17 -0400

    register row for admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import User
-from .models import Table
+from .models import User, Table, Row
 
 admin.site.register(User)
 admin.site.register(Table)
+admin.site.register(Row)
