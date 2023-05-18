7a930ef4b4793c453f5045df3575509140d0fb88
Salmaan Saeed <salsaeed999@gmail.com>
2023-05-18 19:15:39 -0400

    reconfigured the lighthouse config for the vue app in production

--- frontend/lighthouserc.js
+++ frontend/lighthouserc.js
@@ -2,17 +2,13 @@ module.exports = {
     ci: {
       collect: {
         url: [
-          'http://localhost:5173',
+          'http://localhost:4173',
         ],
-        startServerCommand: 'npm run dev',
+        startServerCommand: 'npm run build && npm run preview',
       },
       assert: {
         assertions: {
-          /* Warnings */
-          "categories:performance": ["warn", {"minScore": 0.9}],
-
-          /* Errors */
-          "categories:performance": ["error", {"minScore": 0.5}],
+          "categories:performance": ["error", {"minScore": 0.9}],
           "categories:accessibility": ["error", {"minScore": 0.9}],
           "categories:best-practices": ["error", {"minScore": 0.9}],
           "categories:seo": ["error", {"minScore": 0.9}],
