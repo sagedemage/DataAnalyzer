4f8bc4c1ac4af8354a60116d87ea504d2ccd32f1
Salmaan Saeed <salsaeed999@gmail.com>
2023-05-18 19:35:51 -0400

    added rules to the eslint config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue"
+import App from "./App.vue"
 
 // Vuetify
-import { createVuetify } from 'vuetify'
-import { aliases, mdi } from 'vuetify/iconsets/mdi'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import { createVuetify } from "vuetify"
+import { aliases, mdi } from "vuetify/iconsets/mdi"
+import * as components from "vuetify/components"
+import * as directives from "vuetify/directives"
 
-import './assets/main.css'
+import "./assets/main.css"
 
 const vuetify = createVuetify({
     components,
@@ -20,4 +20,4 @@ const vuetify = createVuetify({
     },
 })
 
-createApp(App).use(vuetify).mount('#app')
+createApp(App).use(vuetify).mount("#app")
